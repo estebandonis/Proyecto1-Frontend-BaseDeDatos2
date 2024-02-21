b70b1b24345b5c2876cb01d4a53ae2e3e3a66327
Esteban Donis <estebandonis10@gmail.com>
2024-02-21 09:39:25 -0600

    hook de api agregado

--- src/pages/Players/Players.jsx
+++ src/pages/Players/Players.jsx
@@ -1,10 +1,22 @@
 import React, { useEffect, useState } from "react";
+import { useApi } from "@hooks";
 import stiles from "./Players.module.css";
 
 const Players = () => {
+  const { loading, data, handleRequest } = useApi();
+
   const handleBackButtonClick = () => {
     window.history.back();
   };
+
+  const reson = async () => {
+    const response = await handleRequest("GET", "/players/find");
+    return response.data;
+  };
+
+  useEffect(() => {
+    console.log(reson());
+  }, []);
 
   return (
     <div className={stiles.bigStyles}>
